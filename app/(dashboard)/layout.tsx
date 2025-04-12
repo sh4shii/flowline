61670deda04a631d54c258a057ca223549fd5b4c
Shashi Kumar <shashisaurav50@gmail.com>
2025-04-12 23:40:42 +0530

    Add theme mode toggle and AppProviders component; update layout to include theme toggle

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import BreadcrumbHeader from "@/components/breadcrumb-header";
 import DesktopSidebar from "@/components/Sidebar";
+import { ThemeModeToogle } from "@/components/theme-mode-toggle";
 import React from "react";
 
 function Layout({ children }: { children: React.ReactNode }) {
@@ -9,6 +10,9 @@ function Layout({ children }: { children: React.ReactNode }) {
       <div className="flex flex-col flex-1 min-h-screen">
         <header className="flex items-center justify-between px-6 py-4 h-[50px] container">
           <BreadcrumbHeader />
+          <div className="gap-1 flex items-center">
+            <ThemeModeToogle />
+          </div>
         </header>
 
         <hr />
---
+++ components/providers/AppProviders.tsx
@@ -0,0 +1,10 @@
+'use client';
+import { ThemeProvider } from 'next-themes';
+
+const AppProviders = ({ children }: { children: React.ReactNode }) => {
+  return <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+    {children}
+  </ThemeProvider>;
+};
+
+export default AppProviders;
